2150e72dbcc2f83cfa24c8a525aede4e85eaeb84
Liang Zhang <psychelzh@outlook.com>
2020-10-18 22:08:35 +0800

    ✨feat(main): add num explosion
    Number of explosion is sometimes said to be a potential index for BART.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/bart.R
+++ R/bart.R
@@ -5,12 +5,13 @@
 #' @param data Raw data of class `data.frame`.
 #' @param ... Other input argument for future expansion.
 #' @return A [tibble][tibble::tibble-package] contains following values:
-#'   \item{mean_hits}{Mean of hits for balloons not exploded.}
-#'   \item{mean_hits_raw}{Mean of hits for all balloons.}
+#'   \item{mean_pumps}{Mean of hits for balloons not exploded.}
+#'   \item{mean_pumps_raw}{Mean of hits for all balloons.}
+#'   \item{num_explosion}{Number of exploded balloons.}
 #'   \item{is_normal}{Checking result whether the data is normal.}
 #' @export
 bart <- function(data, ...) {
-  vars_output <- c("mean_hits", "mean_hits_raw")
+  vars_output <- c("mean_pumps", "mean_pumps_raw", "num_explosion")
   vars_required <- tibble::tribble(
     ~field, ~name,
     "name_nhit", "NHit",
@@ -29,8 +30,9 @@ bart <- function(data, ...) {
   }
   tibble(data) %>%
     dplyr::summarise(
-      mean_hits = mean(.data$NHit[.data$Feedback == 1]),
-      mean_hits_raw = mean(.data$NHit),
-      is_normal = TRUE
+      mean_pumps = mean(.data$NHit[.data$Feedback == 1]),
+      mean_pumps_raw = mean(.data$NHit),
+      num_explosion = sum(.data$Feedback == 0),
+      is_normal = !is.na(.data$mean_pumps)
     )
 }
