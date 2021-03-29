3cfe18032d405be214e6d1814b0b2f93554014bb
Liang Zhang <psychelzh@outlook.com>
2021-03-29 21:10:00 +0800

    🎈perf(utils): use collapse pkg
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/correct_rt_acc.R
+++ R/correct_rt_acc.R
@@ -18,39 +18,31 @@
 #'   because if there is no such correction, the column is added only if the
 #'   corresponding variable exists in the original `data`.
 #' @keywords internal
-correct_rt_acc <- function(data, ...,
+correct_rt_acc <- function(data,
                            crit_rt = 100,
                            correct_type = c("both", "rt", "acc", "none"),
                            name_rt = "RT",
                            name_acc = "ACC") {
   correct_type <- match.arg(correct_type)
-  data_out <- data
+  names_in <- names(data)
   if (correct_type %in% c("both", "rt")) {
-    data_out <- data_out %>%
-      dplyr::mutate(
-        rt_cor = dplyr::if_else(
-          .data[[name_rt]] > crit_rt,
-          .data[[name_rt]], NA_integer_
-        )
-      )
+    rt_vals <- .subset2(data, name_rt)
+    collapse::add_vars(data) <- list(
+      rt_cor = ifelse(rt_vals > crit_rt, rt_vals, NA)
+    )
   }
   if (correct_type %in% c("both", "acc")) {
-    data_out <- data_out %>%
-      dplyr::mutate(
-        acc_cor = dplyr::if_else(
-          .data[[name_rt]] > crit_rt,
-          .data[[name_acc]], -1L
-        )
-      )
+    acc_vals <- .subset2(data, name_acc)
+    collapse::add_vars(data) <- list(
+      acc_cor = ifelse(rt_vals > crit_rt, acc_vals, -1)
+    )
   }
   # add corrected name (with uncorrected value) if name found in original data
-  if (rlang::has_name(data, name_rt) & !rlang::has_name(data_out, "rt_cor")) {
-    data_out <- data_out %>%
-      dplyr::mutate(rt_cor = .data[[name_rt]])
+  if (name_rt %in% names_in & !rlang::has_name(data, "rt_cor")) {
+    collapse::add_vars(data) <- list(rt_cor = .subset2(data, name_rt))
   }
-  if (rlang::has_name(data, name_acc) & !rlang::has_name(data_out, "acc_cor")) {
-    data_out <- data_out %>%
-      dplyr::mutate(acc_cor = .data[[name_acc]])
+  if (name_acc %in% names_in & !rlang::has_name(data, "acc_cor")) {
+    collapse::add_vars(data) <- list(rt_cor = .subset2(data, name_acc))
   }
-  data_out
+  data
 }
