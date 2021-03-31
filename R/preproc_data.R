65780a5c6bdd68027514466ddd19e76c7897b948
Liang Zhang <psychelzh@outlook.com>
2021-03-31 16:14:04 +0800

    🐞fix(high-level): rm collapse deps
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/preproc_data.R
+++ R/preproc_data.R
@@ -36,10 +36,9 @@ preproc_data <- function(data, prep_fun, by = "id", ...) {
     keep.by <- TRUE
   }
   # call the pre-processing function: note the tibble call
-  iq_perf <- prep_fun(data = tibble(data), vars_input = vars_input, by = by)
-  # delete grouping variable if not keeping
-  if (!keep.by) {
-    collapse::get_vars(iq_perf, by) <- NULL
-  }
-  iq_perf
+  prep_fun(data = tibble(data), vars_input = vars_input, by = by) %>%
+    dplyr::select(dplyr::all_of(
+      # keep grouping variable when required
+      c(if (keep.by) by, .get_output_vars(prep_fun_name))
+    ))
 }
