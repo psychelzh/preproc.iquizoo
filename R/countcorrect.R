39079f850127bba8cac0963ddfd99194a27e9504
Liang Zhang <psychelzh@outlook.com>
2021-04-02 00:00:38 +0800

    ✨feat(low-level): add `group`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/countcorrect.R
+++ R/countcorrect.R
@@ -2,52 +2,29 @@
 #'
 #' This is just to find out the count of correct responses.
 #'
-#' @param data Raw data of class `data.frame`.
-#' @param ... Other input argument for future expansion.
+#' @templateVar by low
+#' @templateVar vars_input TRUE
+#' @template params-template
 #' @return A [tibble][tibble::tibble-package] contains following values:
 #'   \item{nc}{Count of correct responses.}
-#'   \item{pc}{Percent of correct responses.}
-#'   \item{is_normal}{Checking result whether the data is normal.}
 #' @export
-countcorrect <- function(data, ...) {
-  vars_output <- c("nc", "pc")
-  vars_required <- tibble::tribble(
-    ~field, ~name,
-    "name_acc", c("ACC", "Repetition", "Correctness")
-  )
-  vars_matched <- match_data_vars(data, vars_required)
-  if (is.null(vars_matched)) {
-    return(compose_abnormal_output(vars_output))
-  }
-  if (utils::hasName(data, "RT")) {
-    data_cor <- correct_rt_acc(
-      data,
-      correct_type = "acc",
-      name_acc = vars_matched["name_acc"]
-    )
-  } else {
-    data_cor <- data %>%
-      dplyr::mutate(acc_cor = .data[[vars_matched["name_acc"]]])
-  }
-  if (is.character(data_cor[["acc_cor"]])) {
-    data_cor <- data_cor %>%
-      dplyr::filter(
-        !is.na(.data[["acc_cor"]]),
-        .data[["acc_cor"]] != "NULL"
+countcorrect <- function(data, by, vars_input) {
+  if (is.character(data[[vars_input[["name_acc"]]]])) {
+    # character input uses "-" to separate individual responses
+    data <- data %>%
+      dplyr::mutate(
+        r"({vars_input[["name_acc"]]})" := purrr::map(
+          .data[[vars_input[["name_acc"]]]],
+          parse_char_acc
+        )
       ) %>%
-      dplyr::summarise(
-        acc_cor = .data[["acc_cor"]] %>%
-          stringr::str_c(collapse = "-") %>%
-          stringr::str_split("-", simplify = TRUE) %>%
-          as.numeric() %>%
-          list()
-      ) %>%
-      tidyr::unnest(.data[["acc_cor"]])
+      tidyr::unnest(.data[[vars_input[["name_acc"]]]])
   }
-  tibble(data_cor) %>%
+  data %>%
+    dplyr::group_by(dplyr::across(dplyr::all_of(by))) %>%
     dplyr::summarise(
-      nc = sum(.data[["acc_cor"]] == 1),
-      pc = mean(.data[["acc_cor"]] == 1),
-      is_normal = TRUE
+      # NA might be produced in parsing characters
+      nc = sum(.data[[vars_input[["name_acc"]]]] == 1, na.rm = TRUE),
+      .groups = "drop"
     )
 }
