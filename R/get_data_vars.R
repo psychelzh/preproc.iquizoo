0109cb2060ffc37d70e675148f72253cad9ad8f4
Liang Zhang <psychelzh@outlook.com>
2021-04-09 13:58:54 +0800

    📃docs(utils): internalize
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/get_data_vars.R
+++ R/get_data_vars.R
@@ -4,14 +4,18 @@
 #' visited directly by any other functions. Here is two functions to get its
 #' "input" and "output" variables.
 #'
-#' @rdname get_vars
+#' @name get_data_vars
 #' @param prep_fun_name The name of the called pre-processing function.
+#' @keywords internal
+NULL
+
+#' @rdname get_data_vars
 .get_input_vars <- function(prep_fun_name) {
   stopifnot(rlang::has_name(config_data_vars, prep_fun_name))
   config_data_vars[[prep_fun_name]][["input"]]
 }
 
-#' @rdname get_vars
+#' @rdname get_data_vars
 .get_output_vars <- function(prep_fun_name) {
   stopifnot(rlang::has_name(config_data_vars, prep_fun_name))
   config_data_vars[[prep_fun_name]][["output"]]
