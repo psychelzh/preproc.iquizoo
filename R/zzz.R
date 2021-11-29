494a0126948332b27a068d12019ec0ba6f1663de
Liang Zhang <psychelzh@outlook.com>
2021-11-29 09:45:52 +0800

    feat(options): add on load options
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,36 @@
 #' @import dplyr
 #' @import tidyr
 NULL
+
+#' Utility functions for data processing for IQUIZOO games.
+#'
+#' @section Package options:
+#'
+#' Preproc.iquizoo uses the following [options()] to configure behaviour:
+#'
+#' \itemize{
+#'   \item `preproc.input`: input variable names, usu. named character.
+#'
+#'   \item `preproc.output`: output variables names.
+#'
+#'   \item `preproc.extra`: extra options such as values for certain input.
+#'
+#' }
+#' @docType package
+#' @keywords internal
+#' @name preproc.iquizoo
+"_PACKAGE"
+
+preproc_default_options <- list(
+  preproc.input = NULL,
+  preproc.output = NULL,
+  preproc.extra = NULL
+)
+
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  toset <- !(names(preproc_default_options) %in% names(op))
+  if (any(toset)) options(preproc_default_options[toset])
+
+  invisible()
+}
