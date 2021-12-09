f01777d43707af32fa1571172fecc6444e864666
Liang Zhang <psychelzh@outlook.com>
2021-12-09 10:28:48 +0800

    fix(switchcost): fix issue of `complete()`
    See more discussion at https://github.com/tidyverse/tidyr/issues/1258
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/utils-switch-congruence.R
+++ R/utils-switch-congruence.R
@@ -20,7 +20,7 @@ calc_switch_cost <- function(data,
                              name_type_switch,
                              name_rt,
                              name_acc) {
-  data |>
+  data_summary <- data |>
     # remove all filler trials
     filter(.data[[name_type_switch]] != "filler") |>
     mutate(
@@ -44,8 +44,16 @@ calc_switch_cost <- function(data,
       mrt = mean(.data[[name_rt]], na.rm = TRUE),
       pc = mean(.data[[name_acc]] == 1),
       .groups = "drop"
-    ) |>
-    complete(.data[["condition"]], nesting(!!!syms(.by))) |>
+    )
+  # weirdly, complete() does not support NULL nesting
+  # https://github.com/tidyverse/tidyr/issues/1258
+  if (is.null(.by)) {
+    data_summary <- complete(data_summary, .data[["condition"]])
+  } else {
+    data_summary <- data_summary |>
+      complete(.data[["condition"]], nesting(!!!syms(.by)))
+  }
+  data_summary |>
     group_by(across(all_of(c(.by, "condition")))) |>
     summarise(
       mrt = mean(.data[["mrt"]], na.rm = TRUE),
