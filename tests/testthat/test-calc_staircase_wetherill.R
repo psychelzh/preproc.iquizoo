134cb3c4cc41c0b7807ff16e6a669037bd92fb28
Liang Zhang <psychelzh@outlook.com>
2023-05-21 16:05:11 +0800

    Add one test case for odd number of reversals
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test-calc_staircase_wetherill.R
+++ tests/testthat/test-calc_staircase_wetherill.R
@@ -1,6 +1,8 @@
 test_that("Correctly find all peaks and valleys", {
   lvls <- c(2:5, 4:1, 2:4)
   expect_equal(calc_staircase_wetherill(lvls), 3)
+  lvls <- c(2:5, 4:1, 2:4, 3:1)
+  expect_equal(calc_staircase_wetherill(lvls), 2.5)
 })
 
 test_that("Return NA if no peaks or valleys", {
