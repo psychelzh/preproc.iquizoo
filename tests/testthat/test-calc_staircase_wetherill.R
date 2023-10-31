d2679c115884b822fd89efa4a45d114c0d8185eb
Liang Zhang <psychelzh@outlook.com>
2023-10-31 15:02:11 +0800

    Fix test glitches
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- tests/testthat/test-calc_staircase_wetherill.R
+++ tests/testthat/test-calc_staircase_wetherill.R
@@ -8,5 +8,6 @@ test_that("Correctly find all peaks and valleys", {
 test_that("Return NA if no peaks or valleys", {
   is.na(calc_staircase_wetherill(c(2:5))) |>
     expect_true() |>
+    expect_warning(class = "no_reversals_found") |>
     expect_warning(class = "no_reversals_found")
 })
